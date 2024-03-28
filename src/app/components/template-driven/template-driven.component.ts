2e9545688559d5a6e85756488619164c4a7e5ffb
josuebaez61@gmail.com <josuebaez61@gmail.com>
2024-03-27 22:24:54 -0300

    ADD, error messages

--- src/app/components/template-driven/template-driven.component.ts
+++ src/app/components/template-driven/template-driven.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-template-driven',
   templateUrl: './template-driven.component.html',
@@ -23,4 +24,21 @@ export class TemplateDrivenComponent {
     email: '',
     contrasena: '',
   };
+
+  onSubmit(ngForm: NgForm): void {
+    console.log(ngForm);
+    if (ngForm.valid) {
+      Swal.fire({
+        title: 'Enviado',
+        icon: 'success',
+      });
+      console.log(ngForm.form.value);
+    } else {
+      ngForm.form.markAllAsTouched();
+      // Swal.fire({
+      //   title: 'Error',
+      //   icon: 'error',
+      // });
+    }
+  }
 }
